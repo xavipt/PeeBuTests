20cf9ae3073939626942cf29e3aa72e79a9b4261
Xavier <2160795@my.ipleiria.pt>
2020-04-04 23:04:40 +0100

    Adding an example test Suite in order to Run automated Tests
    This automated tests are located at the pipeline on Jenkins, this test
    is supposed to run on the pipeline

---
+++ Test Suites/forTesting.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>forTesting</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>edde4ad9-ec60-4290-922e-71634082e091</testSuiteGuid>
+   <testCaseLink>
+      <guid>f5b02b7a-d9df-4172-9e9d-98754f58adbf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/forTesting</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
