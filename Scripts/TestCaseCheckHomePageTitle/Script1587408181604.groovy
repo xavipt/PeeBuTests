1c9d8215329b263cfd0c986c346a5a22bdab54a7
Xavier <2160795@my.ipleiria.pt>
2020-05-13 14:36:39 +0100

    Update do README com os novos servidores e um hot fix a substituir o ip

--- Scripts/TestCaseCheckHomePageTitle/Script1587408181604.groovy
+++ Scripts/TestCaseCheckHomePageTitle/Script1587408181604.groovy
@@ -17,7 +17,7 @@ import internal.GlobalVariable as GlobalVariable
 
 WebUI.openBrowser('')
 
-WebUI.navigateToUrl('http://peebu.test/')
+WebUI.navigateToUrl('http://52.30.237.186/')
 
 WebUI.verifyElementPresent(findTestObject('Page_PeeBu/a_PeeBu'), 0)
 
