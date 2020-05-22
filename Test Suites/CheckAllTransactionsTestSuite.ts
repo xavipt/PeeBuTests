30e6dcdfea8dce20eb24c4a17b3dc1deffab54e5
Xavier <2160795@my.ipleiria.pt>
2020-05-22 19:37:47 +0100

    Ciração do test case do respectivo test suite

---
+++ Test Suites/CheckAllTransactionsTestSuite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>With Data, API tested</description>
+   <name>CheckAllTransactionsTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3e8feb60-3a27-436b-b598-73f9305c3bf6</testSuiteGuid>
+   <testCaseLink>
+      <guid>5c46a338-59f9-44fb-a710-fb7f566a0666</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseCheckAllTransactions</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
