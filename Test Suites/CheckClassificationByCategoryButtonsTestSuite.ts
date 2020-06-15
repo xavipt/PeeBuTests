805384996da48a8910e88dd11cbe69765c82ef50
Xavier <2160795@my.ipleiria.pt>
2020-06-15 02:07:14 +0100

    fix on ClassificationByCategory (feature)

---
+++ Test Suites/CheckClassificationByCategoryButtonsTestSuite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>With Data, API tested</description>
+   <name>CheckClassificationByCategoryButtonsTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>6b5da799-dd08-4f9d-8802-69c892c618f2</testSuiteGuid>
+   <testCaseLink>
+      <guid>3615d55f-fd83-4412-b803-265b9f2902e9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCaseClassificationByCategoryButtons</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
